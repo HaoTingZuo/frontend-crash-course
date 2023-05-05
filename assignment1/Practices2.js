0d77e049254dcf753569a8361fb650d3f09b55e5
HaoTingZuo <131321303+HaoTingZuo@users.noreply.github.com>
2023-05-05 08:52:49 +0800

    Update Practices2.js

--- assignment1/Practices2.js
+++ assignment1/Practices2.js
@@ -1,4 +1,4 @@
-//Practice2-1
+//Practices2-1
 let age = 18;
 
 if (age >= 18) {
@@ -15,7 +15,7 @@ if (age >= 18) {
     console.log('不能投票');
 }
 
-//Practice2-2
+//Practices2-2
 let num = 10;
 
 if (num % 2 === 0) {
@@ -32,13 +32,13 @@ if (num % 2 === 0) {
     console.log(`${num} 是奇數`);
 }
 
-//Practice2-3
+//Practices2-3
 let x = 11;
 let y = 30;
 console.log(x > y);
 console.log(x < y);
 
-//Practice2-4
+//Practices2-4
 let x = 11;
 let y = 30;
 let z = 37;
@@ -46,14 +46,14 @@ console.log(x > y);
 console.log(x > z);
 console.log(y > z);
 
-//Practice2-5
+//Practices2-5
 let min = 10;
 let max = 100;
 let num = 30;
 
 console.log(num > min && num < max);
 
-//Practice2-6
+//Practices2-6
 let year = 2000;
 
 if (year % 4 = 0 && year % 100 = 0 && year % 400 = 0) {
